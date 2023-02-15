166ca294c84d683edfccc5c878d16b78a0f6716a
Mauricio Del Rei <mauricioaraujodelrei@gmail.com>
2023-02-14 22:28:43 -0300

    Communication between microservices with OpenFeign Spring Boot - Development

---
+++ producer-service/src/main/java/com/microsevices/ProducerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.microsevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class ProducerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerServiceApplication.class, args);
+	}
+
+}
